4134b90760e6691c3a0c972759d19a08a25f11f4
ceren kocabaş <ceren.kocabas@hotmail.com>
2026-06-23 10:23:23 +0000

    balance_diff ve expected_newbalance gibi finansal tutar alanları float64 tipine cast edilerek virgülden sonra 2 basamağa yuvarlandı (ROUND).
    orig_balance_error ve dest_balance_error gibi hassas hata payı alanları ham hassasiyeti korunarak float64 tipine dönüştürüldü.
    
    is_ ile başlayan ve tamsayı (0/1) olarak gelen tüm bayrak (flag) alanları dbt standartlarına uygun olarak BOOLEAN tipine cast edildi.

--- models/staging/data426-group3/stg_data426-group3__balance_features.sql
+++ models/staging/data426-group3/stg_data426-group3__balance_features.sql
@@ -10,15 +10,20 @@ renamed as (
 
     select
         transaction_id,
-        balance_diff_orig,
-        balance_diff_dest,
-        expected_newbalanceorig,
-        expected_newbalancedest,
-        orig_balance_error,
-        dest_balance_error,
-        is_sender_balance_zero_after,
-        is_dest_balance_zero_before,
-        is_dest_balance_missing,
+--virgülden sonra 2 basamağa yuvarlanan finanasal alanlar--
+        round(cast(balance_diff_orig as float64),2) as balance_diff_orig,
+        round(cast(balance_diff_dest as float64),2) as balance_diff_dest,
+        round(cast(expected_newbalanceOrig as float64),2) as expected_newbalance_orig,
+        round(cast(expected_newbalanceDest as float64),2) as expected_newbalance_dest,
+
+-- Hassas hata payları (yuvarlamadan,sadece float64 yapıyoruz)--   
+        cast(orig_balance_error as float64) as orig_balance_error,
+        cast(dest_balance_error as float64) as dest_balance_error,
+
+--0/1 İNTEGER lanları BOLEAN yapıyoruz--
+        cast(is_sender_balance_zero_after as boolean) as is_sender_balance_zero_after,
+        cast(is_dest_balance_zero_before as boolean) as is_dest_balance_zero_before,
+        cast(is_dest_balance_missing as boolean ) as is_dest_balance_missing,
         leakage_warning
 
     from source
