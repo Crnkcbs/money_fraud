5d2fbaf401a2d2db93d740d474b0f48d4eec1271
ata1971 <ataberksahin71@gmail.com>
2026-06-24 12:03:38 +0300

    feat: transactions, risk ve fraud tablolarını birleştirme

---
+++ models/mart/mart_data426-group3_risk_fraud_analysis.sql
@@ -0,0 +1,16 @@
+-- mart_data426-group3_risk_fraud_analysis.sql
+SELECT 
+    tf.transaction_id,
+    tf.type,
+    tf.is_fraud,
+    tf.is_flagged_fraud,
+    r.risk_score_rule_based,
+    r.risk_level,
+    r.risk_reason_count,
+    r.high_velocity_flag,
+    r.new_receiver_flag,
+    r.many_to_one_receiver_flag,
+    r.suspicious_cashout_flag,
+    r.large_transfer_flag
+FROM {{ ref('int_data426-group3__transactions_join_fraud_scenario') }} tf
+JOIN {{ ref('stg_data426-group3__risk_features') }} r USING transaction_id
