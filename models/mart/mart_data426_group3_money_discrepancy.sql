00fb7d842c83ada8baf4b515d214cb97c356a48d
Ataberk Şahin <ataberksahin71@gmail.com>
2026-06-30 04:56:29 +0000

    feat: "money_discrepancy.sql" eklendi

---
+++ models/mart/mart_data426_group3_money_discrepancy.sql
@@ -0,0 +1,34 @@
+-- mart_data426-group3__money_discrepancy.sql
+
+WITH transactions AS (
+    SELECT * 
+    FROM {{ ref('int_data426-group3__transactions_join_fraud_scenario') }}
+    WHERE is_merchant_dest = 0
+),
+
+balance_features AS (
+    SELECT * 
+    FROM {{ ref('int_data426-group3__balance_features') }}
+)
+
+SELECT 
+    tf.transaction_id,
+    tf.type,
+    tf.is_fraud,
+    tf.is_flagged_fraud,
+    tf.step,
+    tf.nameorig,
+    tf.amount,
+    tf.old_balance_orig,
+    tf.new_balance_orig,
+    (tf.old_balance_orig - tf.amount - tf.new_balance_orig) AS sender_discrepancy,
+    tf.old_balance_dest,
+    tf.new_balance_dest,
+    (tf.old_balance_dest + tf.amount - tf.new_balance_dest) AS receiver_discrepancy,
+    b.leakage_warning,
+    b.is_sender_balance_zero_after,
+    b.is_dest_balance_zero_before
+
+FROM transactions AS tf
+INNER JOIN balance_features AS b 
+    ON tf.transaction_id = b.transaction_id
